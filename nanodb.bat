1d7217b94d8892a236994f0498bbb1cd26335280
Donnie Pinkston <donnie@cms.caltech.edu>
2019-01-24 19:16:37 -0800

    Update nanodb.bat to work with Maven build
    The nanodb.bat file now behaves in the same way that the *nix nanodb
    script works, so that NanoDB can be started from the Windows cmd shell.

--- nanodb.bat
+++ nanodb.bat
@@ -1,14 +1,22 @@
 @echo off
 
-set CPATH=.
-set CPATH=%CPATH%;lib\log4j-1.2.13.jar
-set CPATH=%CPATH%;lib\antlr-3.2.jar
-set CPATH=%CPATH%;lib\commons-io-2.1.jar
-set CPATH=%CPATH%;lib\commons-lang-2.4.jar
-set CPATH=%CPATH%;build\classes
+set NANODB_SERVER_JAR=target\nanodb-server-0.1-SNAPSHOT.jar
 
-rem To set the page-size to use, add "-Dnanodb.pagesize=2048" to JAVA_OPTS.
-rem To enable transaction processing, add "-Dnanodb.txns=on" to JAVA_OPTS.
-set JAVA_OPTS=-Dlog4j.configuration=logging.conf
+rem Make sure the NanoDB server JAR file has been built
+if not exist %NANODB_SERVER_JAR% (
+    echo Can't find NanoDB server JAR file.  Run 'mvn package' to build the project.
+    exit /b 1
+)
 
-java %JAVA_OPTS% -cp %CPATH% edu.caltech.nanodb.client.ExclusiveClient
+set JAVA_OPTS=-Dlog4j.configurationFile=log4j2.properties
+
+rem Server properties can be specified as system-property arguments.  Examples:
+rem  - To change the default page-size to use, add -Dnanodb.pagesize=2048
+rem  - To enable transaction processing, add -Dnanodb.enableTransactions=on
+rem set JAVA_OPTS=%JAVA_OPTS% -Dnanodb.enableTransactions=on
+
+rem To enable connection to a running server via the IntelliJ IDEA debugger,
+rem uncomment this line:
+rem set JAVA_OPTS=%JAVA_OPTS% -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5009
+
+java %JAVA_OPTS% %* -jar %NANODB_SERVER_JAR%
